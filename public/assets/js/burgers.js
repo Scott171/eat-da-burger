6b8cc10f517407600b8bff5fcb69815d468debc4
Scott171 <K.Scott.Hewitson@gmail.com>
2019-02-21 19:06:01 -0700

    added CSS and image

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,4 +1,4 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
+
 $(function() {
   $(".change-eaten").on("click", function(event) {
     var id = $(this).data("id");
@@ -8,36 +8,35 @@ $(function() {
       eaten: newEaten
     };
 
-    // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newEatenState
     }).then(
       function() {
         console.log("changed eaten to", newEaten);
-        // Reload the page to get the updated list
+       
         location.reload();
       }
     );
   });
 
   $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
+    
     event.preventDefault();
 
     var newBurger = {
       name: $("#ca").val().trim(),
-      eaten: $("[name=eaten]:checked").val().trim()
+      eaten: value="1",
     };
 
-    // Send the POST request.
+  
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
     }).then(
       function() {
         console.log("created new burger");
-        // Reload the page to get the updated list
+        
         location.reload();
       }
     );
@@ -46,13 +45,13 @@ $(function() {
   $(".delete-burger").on("click", function(event) {
     var id = $(this).data("id");
 
-    // Send the DELETE request.
+  
     $.ajax("/api/burgers/" + id, {
       type: "DELETE"
     }).then(
       function() {
         console.log("deleted burger", id);
-        // Reload the page to get the updated list
+       
         location.reload();
       }
     );
